08f973b1b622b94442518ecbdcd5a082d72ccc14
Martin Andráši <andrasi.m007@gmail.com>
2021-03-16 17:06:36 +0100

    ui instalace

--- resources/views/admin/layouts/admin-layout.blade.php
+++ resources/views/admin/layouts/admin-layout.blade.php
@@ -9,7 +9,7 @@
     <link href="{{ mix('/css/admin/admin.css') }}" rel="stylesheet">
 </head>
 
-<body>
+<body class="c-app">
     @if (Session::has('success'))
         <div class="alert alert-success">
             {{ Session::get('success') }}
@@ -20,7 +20,58 @@
             {{ Session::get('error') }}
         </div>
     @endif
-    @yield("container")
+
+    <div class="c-sidebar c-sidebar-dark c-sidebar-show">
+        <ul class="c-sidebar-nav">
+            <li class="c-sidebar-nav-title">Nav Title</li>
+            <li class="c-sidebar-nav-item">
+                <a class="c-sidebar-nav-link" href="#">
+                    <i class="c-sidebar-nav-icon cil-speedometer"></i> Nav item
+                </a>
+            </li>
+            <li class="c-sidebar-nav-item">
+                <a class="c-sidebar-nav-link" href="#">
+                    <i class="c-sidebar-nav-icon cil-speedometer"></i> With badge
+                    <span class="badge badge-primary">NEW</span>
+                </a>
+            </li>
+            <li class="c-sidebar-nav-item nav-dropdown">
+                <a class="c-sidebar-nav-link nav-dropdown-toggle" href="#">
+                    <i class="c-sidebar-nav-icon cil-puzzle"></i> Nav dropdown
+                </a>
+                <ul class="c-sidebar-nav-dropdown-items">
+                    <li class="c-sidebar-nav-item">
+                        <a class="c-sidebar-nav-link" href="#">
+                            <i class="c-sidebar-nav-icon cil-puzzle"></i> Nav dropdown item
+                        </a>
+                    </li>
+                    <li class="c-sidebar-nav-item">
+                        <a class="c-sidebar-nav-link" href="#">
+                            <i class="c-sidebar-nav-icon cil-puzzle"></i> Nav dropdown item
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <button class="c-sidebar-minimizer c-brand-minimizer" type="button"></button>
+    </div>
+
+    <div class="c-wrapper">
+        <header class="c-header">
+            <!-- Header content here -->
+            header
+        </header>
+        <div class="c-body">
+            <main class="c-main">
+                @yield("container")
+            </main>
+        </div>
+        <footer class="c-footer">
+            <!-- Footer content here -->
+        </footer>
+    </div>
+
+
     <script src="{{ mix('/js/admin/admin.js') }}"></script>
 
 
